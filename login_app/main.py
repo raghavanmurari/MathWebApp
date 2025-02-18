3bdb3e1411e9d609906f5336d877c8c1fa8102ca
murari <raghavanmurari@gmail.com>
2025-02-18 11:35:02 +0530

    Updated main.py - the set_page_config() was brought as the first line of code. This was not the case in my previous version as I added new lines of code before  set_page_config()

--- login_app/main.py
+++ login_app/main.py
@@ -4,15 +4,6 @@ from utils.navigation import redirect_to_dashboard, redirect_to_login
 
 from config import get_db
 
-# Diagnostic block to check DB connection
-try:
-    db = get_db()
-    # Attempt a simple query, e.g., list collections or fetch one document
-    collections = db.list_collection_names()
-    st.write("Connected to DB! Collections:", collections)
-except Exception as e:
-    st.write("Error connecting to DB:", e)
-
 # Simple page config with just the basics
 st.set_page_config(
     page_title="Math Web Apppppppp",
@@ -21,6 +12,15 @@ st.set_page_config(
     initial_sidebar_state="collapsed"  # or "auto" or "expanded"
 )
 
+# Diagnostic block to check DB connection
+try:
+    db = get_db()
+    # Attempt a simple query, e.g., list collections or fetch one document
+    collections = db.list_collection_names()
+    st.write("Connected to DB! Collections:", collections)
+except Exception as e:
+    st.write("Error connecting to DB:", e)
+    
 # Initialize session state if not already set
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
